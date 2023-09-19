7c96997da2c298714fb1de58bf94898d76c25599
kotobukid <kotobuki@daqua.jp>
2023-09-19 16:55:12 +0900

    Typescriptで書き換え

--- script.js
+++ script.js
@@ -1,48 +1,54 @@
-const fs = require('fs').promises;
-
-const XlsxPopulate = require("xlsx-populate");
-
-const checkAndCreateDir = async (dirname_relative) => {
-    try {
-        const stat = await fs.stat(dirname_relative);
-    } catch (error) {
-        if (error.code === 'ENOENT') {
-            const created = await fs.mkdir(dirname_relative);
-        } else {
-            throw error;
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import fs from 'node:fs';
+import process from "node:process";
+import XlsxPopulate from "xlsx-populate";
+process.stdin.on('data', (data) => __awaiter(void 0, void 0, void 0, function* () {
+    const checkAndCreateDir = (dirname_relative) => __awaiter(void 0, void 0, void 0, function* () {
+        try {
+            const stat = yield fs.promises.stat(dirname_relative);
+            return;
         }
-    }
-}
-
-process.stdin.on('data', async (data) => {
+        catch (error) {
+            // @ts-ignore
+            if (error.code === 'ENOENT') {
+                const created = yield fs.promises.mkdir(dirname_relative);
+            }
+            else {
+                throw error;
+            }
+        }
+    });
     try {
         // console.log(`Received data from Rust: ${data.toString()}`);
         // process.stdout.write("Hello from Node.js\n");
-
         const json = JSON.parse(data);
         //. テンプレート
         const templateFile = './01.xlsx';
-
-        const dir_created = await checkAndCreateDir("./tmp/");
-
+        const dir_created = yield checkAndCreateDir("./tmp/");
         //. 出力先(Herokuではtmp以下にファイルを一時保存する)
         const outputFilePath = './tmp/' + json.excelName + '.xlsx';
-
-        const book = await XlsxPopulate.fromFileAsync(templateFile)
-
+        const book = yield XlsxPopulate.fromFileAsync(templateFile);
         // 0番目のsheetに書き込む
         const sheet1 = book.sheet(0);
         for (const [key, value] of Object.entries(json.cell)) {
+            // @ts-ignore
             sheet1.cell(key).value(value);
         }
-        const result = await book.toFileAsync(outputFilePath);
-
+        const result = yield book.toFileAsync(outputFilePath);
         process.stdout.write(outputFilePath);
         // process.stdout.write(result);
-
         process.exit(0);
-    } catch (e) {
+    }
+    catch (e) {
         console.error(e);
         process.exit(1);
     }
-});
+}));
